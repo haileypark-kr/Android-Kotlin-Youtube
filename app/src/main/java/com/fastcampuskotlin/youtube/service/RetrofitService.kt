a7c13aa8eb759294ae4b37fc395997e9e28c1f09
박수현 <soohyun.park@kt.com>
2022-08-08 16:43:04 +0900

    상단 타이틀 숨기기

--- app/src/main/java/com/fastcampuskotlin/youtube/service/RetrofitService.kt
+++ app/src/main/java/com/fastcampuskotlin/youtube/service/RetrofitService.kt
@@ -5,7 +5,11 @@ import retrofit2.http.GET
 
 
 class YoutubeItem(
-	val id: Int, val title: String, val content: String, val video: String, val thumbnail: String
+	val id: Int,
+	val title: String,
+	val content: String,
+	val video: String,
+	val thumbnail: String
 )
 
 interface RetrofitService {
